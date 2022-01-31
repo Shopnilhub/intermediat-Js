04f43eccad71b488868d65904ad8050a32e97e71
Abrar Alif Shopnil <abrar.alif1994@gmail.com>
2022-02-01 00:12:22 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// Falsy list
+// 0
+// ""
+// NaN
+// undefined
+// null
+// Truthy
+// "0", ' ', [],{};
+let name = 12;
+if(name || name==0){
+    console.log("Condition is true")
+}
+else{
+    console.log("Condition is false")
+}
